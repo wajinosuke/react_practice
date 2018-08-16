9ff2be1332c41d09688a1a06abd676f1122d9033
wajinosuke <w2physalis+github@gmail.com>
2018-08-16 15:33:58 +0900

    手動でWebpack環境作成練習（その２）

---
+++ test_react_webpack/src/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { Comp } from './Comp';
+
+const Test = (props) => {
+    return (
+        <div>hoge</div>
+    );
+}
+
+ReactDOM.React(
+    <Test />,
+    document.getElementById('root')
+);
---
+++ test_react_webpack/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: './bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }]
+    }
+};
